d7c3d405de2ce7d92ef74adb82b8f3df617ec718
ShaikhMuhammadFaizan <shkfaizan19@gmail.com>
2020-02-16 03:21:46 +0500

    Header benefits card

--- src/Benefits.jsx
+++ src/Benefits.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Col, Row, Button, Image,Card, CardDeck } from 'react-bootstrap';
+import { Container, Col, Row, Button, Image, Card, CardDeck, } from 'react-bootstrap';
 import BenefitImg1 from './Assets/reactJS.jpg'
 
 
@@ -12,39 +12,23 @@ class Benefits extends Component {
                 <Col className="color-white-text benefits-area-elements">
                     <p className="heading-text">What will you <span className="interest-text"> learn? </span></p>
                     <p className="supporting-desc-text">A supporting statement for your vakye proposition to encourage customers to complete you CTA</p>
-                    <Row className="course-benefit-area">
-                            <CardDeck className="course-benefit-desc-area">
-                                <Card className=" d-flex justify-content-center align-items-center">
-                                    <Card.Img variant="top" src={BenefitImg1} style={{width:'100px',height:'100px'}}/>
-                                    <Card.Body>
-                                        <Card.Title>Benefit No.1</Card.Title>
-                                        <Card.Text className="benefits-desc">
-                                            Add a description of your offer and key benefits how it can attract customers    
-                                        </Card.Text>
-                                    </Card.Body>
-                                   
-                                </Card>
-                                <Card>
-                                    <Card.Img variant="top" src={BenefitImg1} />
-                                    <Card.Body>
-                                        <Card.Title>Benefit No.2</Card.Title>
-                                        <Card.Text className="benefits-desc">
-                                        Add a description of your offer and key benefits how it can attract customers
-                                        </Card.Text>
-                                    </Card.Body>
-                                   
-                                </Card>
-                                <Card>
-                                    <Card.Img variant="top" src={BenefitImg1} />
-                                    <Card.Body>
-                                        <Card.Title>Benefit No.3</Card.Title>
-                                        <Card.Text className="benefits-desc">
-                                        Add a description of your offer and key benefits how it can attract customers</Card.Text>
-                                    </Card.Body>
-                                   
-                                </Card>
-                            </CardDeck>
-                        </Row>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex justify-content-center align-items-center benefit-box">
+                            <Image src={BenefitImg1} style={{width:'70px',height:'70px'}}/>
+                            <p className="benefit-text">Benefit <span className="benefit-num-text">1</span> </p>
+                            <p className="benefit-desc-text">Add a description of your offer and key benifits what it is and how it helps</p>
+                        </div>
+                        <div className="d-flex justify-content-center align-items-center benefit-box blue-box">
+                            <Image src={BenefitImg1} style={{width:'70px',height:'70px'}}/>
+                            <p className="benefit-text">Benefit <span className="benefit-num-text">1</span> </p>
+                            <p className="benefit-desc-text">Add a description of your offer and key benifits what it is and how it helps</p>
+                        </div>
+                        <div className="d-flex justify-content-center align-items-center benefit-box">
+                            <Image src={BenefitImg1} style={{width:'70px',height:'70px'}}/>
+                            <p className="benefit-text">Benefit <span className="benefit-num-text">1</span> </p>
+                            <p className="benefit-desc-text">Add a description of your offer and key benifits what it is and how it helps</p>
+                        </div>
+                    </div>
 
                 </Col>
             </Container>
