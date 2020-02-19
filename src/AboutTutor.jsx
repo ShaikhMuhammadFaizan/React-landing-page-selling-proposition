6c79d16396b15fbb73f9d39d270584bb64461388
ShaikhMuhammadFaizan <shkfaizan19@gmail.com>
2020-02-19 20:56:10 +0500

    Abouttutor component structure

---
+++ src/AboutTutor.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './AboutTutor.css';
+
+class Abouttutor extends Component {
+  render() {
+    return (
+      <div className="about-tutor-area">
+        rtjkthkdjh
+      </div>
+    );
+  }
+}
+
+
+export default Abouttutor;
--- src/Benefits.jsx
+++ src/Benefits.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Col, Row, Button, Image, Card, CardDeck, } from 'react-bootstrap';
+import { Container, Col, Image, } from 'react-bootstrap';
 import BenefitImg1 from './Assets/reactJS.jpg'
 
 
@@ -11,22 +11,22 @@ class Benefits extends Component {
             <Container className="benefits-area d-flex justify-content-center align-items-center">
                 <Col className="color-white-text benefits-area-elements">
                     <p className="heading-text">What will you <span className="interest-text"> learn? </span></p>
-                    <p className="supporting-desc-text">A supporting statement for your vakye proposition to encourage customers to complete you CTA</p>
-                    <div className="d-flex justify-content-between align-items-center">
+                    <p className="supporting-desc-text">A supporting statement for your value proposition to encourage customers to complete you CTA</p>
+                    <div className="d-flex justify-content-between align-items-center benefit-box-area">
                         <div className="d-flex justify-content-center align-items-center benefit-box">
                             <Image src={BenefitImg1} style={{width:'70px',height:'70px'}}/>
-                            <p className="benefit-text">Benefit <span className="benefit-num-text">1</span> </p>
-                            <p className="benefit-desc-text">Add a description of your offer and key benifits what it is and how it helps</p>
+                            <p className="benefit-text benefit-heading-text benefit-odd">Benefit <span className="benefit-num-text-lt-blue">1</span> </p>
+                            <p className="benefit-desc-text benefit-odd benefit-desc">Add a description of your offer and key benifits what it is and how it helps</p>
                         </div>
                         <div className="d-flex justify-content-center align-items-center benefit-box blue-box">
                             <Image src={BenefitImg1} style={{width:'70px',height:'70px'}}/>
-                            <p className="benefit-text">Benefit <span className="benefit-num-text">1</span> </p>
-                            <p className="benefit-desc-text">Add a description of your offer and key benifits what it is and how it helps</p>
+                            <p className="benefit-text benefit-heading-text color-white-text benefit-even">Benefit <span className="benefit-num-text-dr-blue">2</span> </p>
+                            <p className="benefit-desc-text benefit-even benefit-desc">Add a description of your offer and key benifits what it is and how it helps</p>
                         </div>
                         <div className="d-flex justify-content-center align-items-center benefit-box">
                             <Image src={BenefitImg1} style={{width:'70px',height:'70px'}}/>
-                            <p className="benefit-text">Benefit <span className="benefit-num-text">1</span> </p>
-                            <p className="benefit-desc-text">Add a description of your offer and key benifits what it is and how it helps</p>
+                            <p className="benefit-text benefit-heading-text benefit-odd">Benefit <span className="benefit-num-text-lt-blue">3</span> </p>
+                            <p className="benefit-desc-text benefit-odd benefit-desc">Add a description of your offer and key benifits what it is and how it helps</p>
                         </div>
                     </div>
 
@@ -35,6 +35,5 @@ class Benefits extends Component {
         );
     }
 }
-
 
 export default Benefits;
